c5a40f626375c202b4f1bab93b42ac91bd4339f6
Jan Gräfen <paradox_haze@live.de>
2020-09-20 23:22:56 +0200

    Add install test

--- pkg/manager/install_test.go
+++ pkg/manager/install_test.go
@@ -1,7 +1,9 @@
 package manager
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"runtime"
 	"testing"
 
@@ -16,16 +18,22 @@ func TestGoManager_Install(t *testing.T) {
 	validVersion := version.Must(version.NewVersion("1.15.2"))
 	invalidVersion := version.Must(version.NewVersion("42.1337.3"))
 
+	tempDir := t.TempDir()
+
 	sut, err := NewManager(&tasks.Task{
 		ErrorExitCode: 1,
 		Output:        os.Stdout,
 		Error:         os.Stderr,
-	}, t.TempDir())
+	}, tempDir)
 
 	assert.NoError(t, err)
 	assert.NotNil(t, sut)
 
 	assert.Error(t, sut.Install(invalidVersion, runtime.GOOS, runtime.GOARCH, releases.IncludeAll))
+
 	assert.NoError(t, sut.Install(validVersion, runtime.GOOS, runtime.GOARCH, releases.IncludeAll))
+	assert.DirExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s", validVersion)))
+
 	assert.Error(t, sut.Install(validVersion, runtime.GOOS, runtime.GOARCH, releases.IncludeAll))
+	assert.DirExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s", validVersion)))
 }
