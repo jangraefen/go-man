5d7ae1f1c4e2876f7d75beb7ef126d3bd5b6dc54
Jan Gräfen <paradox_haze@live.de>
2020-09-20 23:34:08 +0200

    Add cleanup test

---
+++ pkg/manager/cleanup_test.go
@@ -0,0 +1,42 @@
+package manager
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/hashicorp/go-version"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/NoizeMe/go-man/pkg/releases"
+	"github.com/NoizeMe/go-man/pkg/tasks"
+)
+
+func TestGoManager_Cleanup(t *testing.T) {
+	stableRelease, err := releases.GetLatest(releases.IncludeStable)
+	assert.NoError(t, err)
+
+	stableVersion := stableRelease.GetVersionNumber()
+	unstableVersion := version.Must(version.NewVersion("1.11.0"))
+
+	tempDir := t.TempDir()
+
+	setupInstallation(t, tempDir, stableVersion)
+	setupInstallation(t, tempDir, unstableVersion)
+
+	sut, err := NewManager(&tasks.Task{
+		ErrorExitCode: 1,
+		Output:        os.Stdout,
+		Error:         os.Stderr,
+	}, tempDir)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, sut)
+
+	assert.NoError(t, sut.Cleanup())
+	assert.DirExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s", stableVersion)))
+	assert.FileExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s-amd64-windows.zip", stableVersion)))
+	assert.NoDirExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s", unstableVersion)))
+	assert.NoFileExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s-amd64-windows.zip", unstableVersion)))
+}
