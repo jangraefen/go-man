9391a40ee9dc76a007beea7ca5b375fd8ceb394e
Jan Gräfen <paradox_haze@live.de>
2020-09-21 21:52:35 +0200

    Add selecting to uninstall test

--- pkg/manager/uninstall_test.go
+++ pkg/manager/uninstall_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/NoizeMe/go-man/pkg/tasks"
+	"github.com/NoizeMe/go-man/pkg/utils"
 )
 
 func TestGoManager_UninstallAll(t *testing.T) {
@@ -53,7 +54,7 @@ func TestGoManager_Uninstall(t *testing.T) {
 	sut := &GoManager{
 		RootDirectory:     tempDir,
 		InstalledVersions: version.Collection{validVersion},
-		SelectedVersion:   nil,
+		SelectedVersion:   validVersion,
 		task: &tasks.Task{
 			ErrorExitCode: 1,
 			Output:        os.Stdout,
@@ -61,16 +62,26 @@ func TestGoManager_Uninstall(t *testing.T) {
 		},
 	}
 	assert.NotNil(t, sut)
+	assert.NoError(t, link(
+		filepath.Join(tempDir, fmt.Sprintf("go%s", validVersion)),
+		filepath.Join(tempDir, selectedDirectoryName),
+	))
 
 	assert.Error(t, sut.Uninstall(invalidVersion))
 
 	assert.NoError(t, sut.Uninstall(validVersion))
 	assert.NoDirExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s", validVersion)))
 	assert.NoFileExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s-windows-amd64.zip", validVersion)))
+	assert.False(t, utils.PathExists(filepath.Join(tempDir, selectedDirectoryName)))
 
-	assert.Error(t, sut.Uninstall(validVersion))
+	sut.InstalledVersions = version.Collection{validVersion}
+	setupInstallation(t, tempDir, validVersion)
+
+	assert.NoError(t, sut.Uninstall(validVersion))
 	assert.NoDirExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s", validVersion)))
 	assert.NoFileExists(t, filepath.Join(tempDir, fmt.Sprintf("go%s-windows-amd64.zip", validVersion)))
+
+	assert.Error(t, sut.Uninstall(validVersion))
 }
 
 func setupInstallation(t *testing.T, rootDirectory string, goVersion *version.Version) {
