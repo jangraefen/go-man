a130e229ba2547ffadfaca2db7ad3d95af571629
Jan Gräfen <paradox_haze@live.de>
2020-09-21 21:02:56 +0200

    Add test for fake client

--- pkg/utils/http_test.go
+++ pkg/utils/http_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"io/ioutil"
 	"path/filepath"
 	"testing"
 
@@ -35,4 +37,25 @@ func TestGetFile(t *testing.T) {
 	downloaded, err = GetFile("https://golang.org/dl/?mode=json", destinationFile, true)
 	assert.NoError(t, err)
 	assert.True(t, downloaded)
+}
+
+func TestStaticResponseClient(t *testing.T) {
+	sut := StaticResponseClient(404, []byte("not found"), nil)
+
+	response, err := sut.Get("http://example.org")
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, 404, response.StatusCode)
+
+	body, err := ioutil.ReadAll(response.Body)
+	_ = response.Body.Close()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("not found"), body)
+
+	sut = StaticResponseClient(0, nil, errors.New("failure"))
+
+	response, err = sut.Get("http://example.org")
+	assert.Error(t, err)
+	assert.Nil(t, response)
+	assert.Equal(t, `Get "http://example.org": failure`, err.Error())
 }
