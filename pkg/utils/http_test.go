76d576a22bdc754c00cbafea0d957a0e9052a240
Jan Gräfen <paradox_haze@live.de>
2020-09-21 21:35:27 +0200

    Improve http tests

--- pkg/utils/http_test.go
+++ pkg/utils/http_test.go
@@ -3,26 +3,62 @@ package utils
 import (
 	"errors"
 	"io/ioutil"
+	"net/http"
+	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetJSON(t *testing.T) {
+	t.Cleanup(func() {
+		Client = http.DefaultClient
+	})
+
 	var collection interface{} = nil
+
 	assert.Error(t, GetJSON("http://example.org/404.txt", &collection))
 	assert.Nil(t, collection)
 
 	assert.NoError(t, GetJSON("https://golang.org/dl/?mode=json", &collection))
 	assert.NotNil(t, collection)
+
+	Client = StaticResponseClient(404, []byte("not found"), nil)
+	collection = nil
+
+	assert.Error(t, GetJSON("http://example.org/404.txt", &collection))
+	assert.Nil(t, collection)
+
+	Client = StaticResponseClient(200, []byte("not json"), nil)
+
+	assert.Error(t, GetJSON("http://example.org/404.txt", &collection))
+	assert.Nil(t, collection)
+
+	Client = StaticResponseClient(0, nil, errors.New("failure"))
+
+	assert.Error(t, GetJSON("http://example.org/404.txt", &collection))
+	assert.Nil(t, collection)
 }
 
 func TestGetFile(t *testing.T) {
 	tempDir := t.TempDir()
 	destinationFile := filepath.Join(tempDir, "destination.txt")
+	rootFile := getNoPermissionDirectory("destination.txt")
+
+	t.Cleanup(func() {
+		Client = http.DefaultClient
+	})
+	t.Cleanup(func() {
+		_ = os.Remove(rootFile)
+	})
 
 	downloaded, err := GetFile("http://example.org/404.txt", destinationFile, false)
+	assert.Error(t, err)
+	assert.True(t, downloaded)
+
+	downloaded, err = GetFile("https://golang.org/dl/?mode=json", rootFile, false)
 	assert.Error(t, err)
 	assert.True(t, downloaded)
 
@@ -36,6 +72,12 @@ func TestGetFile(t *testing.T) {
 
 	downloaded, err = GetFile("https://golang.org/dl/?mode=json", destinationFile, true)
 	assert.NoError(t, err)
+	assert.True(t, downloaded)
+
+	Client = StaticResponseClient(0, nil, errors.New("failure"))
+
+	downloaded, err = GetFile("https://golang.org/dl/?mode=json", destinationFile, true)
+	assert.Error(t, err)
 	assert.True(t, downloaded)
 }
 
@@ -54,8 +96,16 @@ func TestStaticResponseClient(t *testing.T) {
 
 	sut = StaticResponseClient(0, nil, errors.New("failure"))
 
-	response, err = sut.Get("http://example.org")
+	response, err = sut.Get("http://example.org") //nolint:bodyclose
 	assert.Error(t, err)
 	assert.Nil(t, response)
 	assert.Equal(t, `Get "http://example.org": failure`, err.Error())
+}
+
+func getNoPermissionDirectory(fileName string) string {
+	if runtime.GOOS == "windows" {
+		return filepath.Join(filepath.VolumeName("/"), fileName)
+	}
+
+	return filepath.Join("/", fileName)
 }
