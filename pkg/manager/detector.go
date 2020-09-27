a88ab3a568aa72b6ba1d0f65e31a8097864e0e1a
Jan Gräfen <paradox_haze@live.de>
2020-09-27 22:32:38 +0200

    Remove unnecessary check

--- pkg/manager/detector.go
+++ pkg/manager/detector.go
@@ -1,22 +1,15 @@
 package manager
 
 import (
-	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
 
 	"github.com/hashicorp/go-version"
-
-	"github.com/NoizeMe/go-man/internal/fileutil"
 )
 
 func detectGoVersion(sdkDirectory string) (*version.Version, error) {
 	versionPath := filepath.Join(sdkDirectory, "VERSION")
-
-	if !fileutil.PathExists(versionPath) {
-		return nil, fmt.Errorf("could not locate version file: %s", versionPath)
-	}
 
 	versionContent, err := ioutil.ReadFile(versionPath)
 	if err != nil {
